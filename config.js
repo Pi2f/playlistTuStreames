8c0ec631dd195b5d531539a3082d3f53bdd8485e
Pi2f <pierre.franc.gap@gmail.com>
2019-01-18 09:21:10 +0100

    heroku db

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const uuidv4 = require('uuid/v4');
 module.exports = {
     // 'urlDB' : 'mongodb://192.168.99.100:32768/Playlists',
-    'urlDB' : 'mongodb://tu-streames1:tu-streames1@ds153974.mlab.com:53974/tu-streames',
+    'urlDB' : 'mongodb://tu-streames1:tu-streames1@ds161024.mlab.com:61024/tustreames-playlist',
     // 'urlDB' : 'mongodb://database/TuStreames',
     'port' : "3003",
 }
